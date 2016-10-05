8be018b1053477b422ac90b85cd312a25b29c413
Ben Kannenberg <bkannenberg@gmail.com>
2016-10-05 15:31:35 +1030

    Refactor gadget js/css

--- src/javascripts/bookeasy-mods.js
+++ src/javascripts/bookeasy-mods.js
@@ -1,64 +1,57 @@
+IMUtility.detailsGadgetGridRendered = false;
+
 jQuery(document).on('gadget.script.loaded', function() {
+    $w.event.subscribe('grid.rendered', function() {
 
-    IMUtility.pushDetailsGadgetLoadedEvent();
-    IMUtility.pushDetailsContentLoadedEvent();
+        // Do not run this event handler more than once
+        if (IMUtility.detailsGadgetGridRendered) return true;
+        IMUtility.detailsGadgetGridRendered = true;
 
-    $w.event.subscribe('details.gadget.ready', function() {
-
-        jQuery('.im-grid tr.odd, .im-grid tr.even').each(function() {
-            // setup fancybox
-            thumbImage = jQuery(this).find('.thumb img');
-            imagePath = thumbImage.attr('rel');
-
-            thumbImage.attr('src', imagePath);
-
-            thumbImage.wrap('<a class="be-fancybox" href="' + imagePath + '" rel="gallery" title="' + jQuery(this).find('a:not([class])').text() + '"></a>')
+        jQuery('.im-grid tbody tr').each(function() {
+            jQuery(this).find('td.name').addClass('im-grid__details').removeClass('name');
+            // jQuery(this).find('td.price').addClass('hidden-xs');
+            // jQuery(this).find('td.price:gt(3)').addClass('hidden-sm');
+            // jQuery(this).find('td.price:gt(7)').addClass('hidden-md');
         });
 
-    });
+        // Insert named containers for row details
+        jQuery('.im-grid__details .thumb').after('<div class="im-grid-roominfo"><h3 class="im-grid-roominfo__roomname"></h3><div class="im-grid-roominfo__actionbar"></div></div>');
+
+        // add class to grid rows
+        jQuery('.im-grid tbody tr').addClass('im-grid-item');
+
+        // Insert row details into named containers..
+        jQuery('.im-grid-item').each(function() {
+            if (IMUtility.debug) console.log('Next Row: '+ jQuery(this).find('td.im-grid__details a').text() );
+
+            // remove unwanted elements
+            jQuery(this).find('.OperatorItemHeading, .OperatorInfoMore').remove();
+
+            // always display description
+            jQuery(this).find('.OperatorInfo').addClass('im-grid-operatorinfo').removeClass('.OperatorInfoHidden, .OperatorInfo');
+
+            jQuery(this).find('td.im-grid__details a').appendTo(jQuery(this).find('.im-grid-roominfo__roomname'));
+            jQuery(this).find('td.im-grid__details .specials').appendTo(jQuery(this).find('.im-grid-roominfo__actionbar'));
+        });
 
 
-    $w.event.subscribe('details.content.ready', function() {
+        if (jQuery('.im-grid thead tr td.thumb').length>0 || jQuery('.im-grid tbody tr td.thumb').length>0) { return true; }
+        jQuery('.im-grid thead tr').prepend('<td class="thumb"></td>');
+        jQuery('.im-grid thead tr .title').empty();
 
-        jQuery('.priceGrid').addClass('is-loading');
+        jQuery('.im-grid tbody tr').each(function() {
 
-        // format OperatorInfo
-        jQuery('.im-grid tr.odd, .im-grid tr.even').each(function() {
-            operatorInfo = jQuery(this).find('.OperatorInfo');
+            // Move thumb to its own column & add fancybox
+            $thumbImage = jQuery(this).find('.thumb > img');
+            imagePath = $thumbImage.attr('rel');
 
-            jQuery(this).find('.RoomConfig, .MaxGuests, .OperatorItemHeading, .OperatorInfoMore').remove();
+            $thumbImage.attr('src', imagePath).wrap('<a class="be-fancybox" href="' + imagePath + '" rel="gallery" title="' + jQuery(this).find('a:not([class])').text() + '"></a>');
 
-            operatorInfo.removeClass('OperatorInfoHidden');
-
-            // // remove unwanted elements
-            // jQuery(this).find('.RoomConfig, .MaxGuests, .OperatorItemHeading, .OperatorInfoMore').remove();
-            //
-            // jQuery(this).find('a:not([class])').wrap('<h3>');
-            //
-            operatorInfo.prepend(jQuery(this).find('h3'));
-
-            // find in array
-            currentName = jQuery(this).find('a:not([class])').text();
-
-            // check if it can find the currentName
-            if(typeof aTourData !== 'undefined' && typeof aTourData[currentName] !== 'undefined') {
-
-                sDescription = aTourData[currentName].replace(/\n/g, '<br />');
-
-                sDescription = sDescription.replace(/\b(FACILITY|BBQ|SHELTER|SEATING|TOILETS|HOLDING CAPACITY|CAR PARKS|ALTERNATIVE PARKING|WATER AVAILABLE|POWER|TENNIS COURTS|WHEELCHAIR ACCESS|SHADE)\b/g,'<strong>$1</strong>');
-
-                // replace description
-                jQuery(this).find('.Description').html('<p>' + sDescription + '</p>');
-
-
-            }
-
+            jQuery(this).find('td.im-grid__details').before('<td class="thumb"></td>');
+            jQuery(this).find('td.thumb').append(jQuery(this).find('div.thumb'));
 
         });
 
-        jQuery('.priceGrid').removeClass('is-loading');
-
 
     });
-
 });
