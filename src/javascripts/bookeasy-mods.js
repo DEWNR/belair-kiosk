9a00ed68ba11fa9c7d79e6cbba4d2bddeb78953d
Ben Kannenberg <bkannenberg@gmail.com>
2016-10-26 16:45:04 +1030

    keep the static design

--- src/javascripts/bookeasy-mods.js
+++ src/javascripts/bookeasy-mods.js
@@ -1,80 +1,53 @@
 IMUtility.detailsGadgetGridRendered = false;
 
-jQuery(document).on('gadget.script.loaded', function() {
+$(document).on('gadget.script.loaded', function() {
+
     $w.event.subscribe('grid.rendered', function() {
 
         // Do not run this event handler more than once
         if (IMUtility.detailsGadgetGridRendered) return true;
         IMUtility.detailsGadgetGridRendered = true;
 
-        jQuery('.im-grid table tbody tr:first-child td.name a.more').IMElementExists(function() {
+
+        $('.im-grid table tbody tr:first-child td.name a.more').IMElementExists(function() {
             // Wait for room details link insertion before moving the row data
-            arrangeRowData();
+            insertImages('details');
 
         });
 
+    });
 
-        function arrangeRowData() {
-
-            thumbCount = 0;
-
-            jQuery('.im-grid tbody tr').each(function() {
-                jQuery(this).find('td.name').addClass('im-grid__details').removeClass('name');
-            });
-
-            // Insert named containers for row details
-            jQuery('.im-grid__details .OperatorInfoMore').before('<div class="im-grid-roominfo"><h3 class="im-grid-roominfo__roomname"></h3><div class="im-grid-roominfo__actionbar"></div></div>');
-
-            // add class to grid rows
-            jQuery('.im-grid tbody tr').addClass('im-grid-item');
+});
 
 
-            // Insert row details into named containers..
-            jQuery('.im-grid-item').each(function() {
-                if (IMUtility.debug) console.log('Next Row: '+ jQuery(this).find('td.im-grid__details a').text() );
-
-                // remove unwanted elements
-                jQuery(this).find('.OperatorItemHeading, .OperatorInfoMore').remove();
-
-                // always display description
-                jQuery(this).find('.OperatorInfo').addClass('im-grid-operatorinfo').removeClass('.OperatorInfoHidden, .OperatorInfo');
-
-                jQuery(this).find('td.im-grid__details a').appendTo(jQuery(this).find('.im-grid-roominfo__roomname'));
-                jQuery(this).find('td.im-grid__details .specials').appendTo(jQuery(this).find('.im-grid-roominfo__actionbar'));
-            });
 
 
-            if (jQuery('.im-grid thead tr td.thumb').length>0 || jQuery('.im-grid tbody tr td.thumb').length>0) { return true; }
 
-            jQuery('.im-grid thead tr').prepend('<td class="thumb"></td>');
-            jQuery('.im-grid thead tr .title').empty();
+function insertImages(gadget) {
+    var thumbCount = 0;
 
-            jQuery('.im-grid tbody tr').each(function() {
+    $('.im-grid tbody tr').each(function() {
 
-                if (jQuery(this).find('td.im-grid__details div.thumb').size()) {
+        if ($(this).find('td.name div.thumb').size()) {
 
-                    // Move thumb to its own column & add fancybox
-                    $thumbImage = jQuery(this).find('.thumb > img');
-                    imagePath = $thumbImage.attr('rel');
-                    thumbCount = thumbCount + 1;
+            // Move thumb to its own column & add fancybox
+            $thumbImage = $(this).find('.thumb > img');
+            imagePath = $thumbImage.attr('rel');
 
-                    $thumbImage.attr('src', imagePath).wrap('<a class="be-fancybox" href="' + imagePath + '" rel="gallery" title="' + jQuery(this).find('a:not([class])').text() + '"></a>');
+            thumbCount = thumbCount + 1;
 
-                    jQuery(this).find('td.im-grid__details').before('<td class="thumb"></td>');
-                    jQuery(this).find('td.thumb').append(jQuery(this).find('div.thumb'));
+            $thumbImage.attr('src', imagePath).wrap('<a class="be-fancybox" href="' + imagePath + '" rel="gallery" title="' + $(this).find('a:not([class])').text() + '"></a>');
 
-                }
 
-            });
-
-            if (thumbCount == 0) {
-                // remove header thumb
-
-                jQuery('.im-grid thead tr td.thumb').remove();
-            }
-
+        } else {
+            $(this).addClass('no-image');
         }
 
-
     });
-});
+
+    if (thumbCount == 0) {
+        // remove header thumb
+
+        $('.im-grid thead tr td.thumb').remove();
+    }
+}
